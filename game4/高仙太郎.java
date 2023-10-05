7d9e7d0555306a7e79b1e50c951d96df4f8ddd0c
RuaSu <19088@hakodate.kosen-ac.jp>
2023-10-05 10:16:40 +0900

    Dがキャラ追加

---
+++ game4/高仙太郎.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class 高仙太郎 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class 高仙太郎 extends Actor
+{
+    /**
+     * Act - do whatever the 高仙太郎 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
