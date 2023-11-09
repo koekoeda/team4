90bf5e55f0349a98b5a52cf13aa55e1a7982753b
ichimi0730 <19084@hakodate.kosen-ac.jp>
2023-11-09 10:12:43 +0900

    a

--- game4/MyWorld.java
+++ game4/MyWorld.java
@@ -16,6 +16,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1000, 667, 1); 
+        addObject(new 高仙太郎(), 100, 100);
     }
 }
