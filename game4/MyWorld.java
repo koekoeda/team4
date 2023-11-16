82039e6a78326b70ff79165d98793a0ebf9ff6ab
ichimi0730 <19084@hakodate.kosen-ac.jp>
2023-11-16 10:24:01 +0900

    画像の変更

--- game4/MyWorld.java
+++ game4/MyWorld.java
@@ -14,11 +14,28 @@ public class MyWorld extends World
      * 
      */
     private GreenfootImage Sgmap = null;
+    private GreenfootImage Meter = null;
+    private GreenfootImage MeterBox = null;
     public MyWorld()
     {
         super(1000,655,1);
         Sgmap = new GreenfootImage( "images/Sgmap.png" );
         Sgmap.scale(800, 500);
         getBackground().drawImage( Sgmap, 100, 50 );
+        MeterBox = new GreenfootImage( "images/好感度メータ.png" );
+        MeterBox.scale( 200, 40 );
+        Meter = new GreenfootImage( "images/好感度.png" );
+        Meter.scale( 22, 40 );
+        getBackground().drawImage( MeterBox, 50, 50 );
+        getBackground().drawImage( Meter, 50, 50 );//好感度1
+        getBackground().drawImage( Meter, 70, 50 );//好感度2
+        getBackground().drawImage( Meter, 90, 50 );//好感度3
+        getBackground().drawImage( Meter, 110, 50 );//好感度4
+        getBackground().drawImage( Meter, 130, 50 );//好感度5
+        getBackground().drawImage( Meter, 150, 50 );//好感度6
+        getBackground().drawImage( Meter, 170, 50 );//好感度7
+        getBackground().drawImage( Meter, 190, 50 );//好感度8
+        getBackground().drawImage( Meter, 210, 50 );//好感度9
+        getBackground().drawImage( Meter, 230, 50 );//好感度10
     }
 }
