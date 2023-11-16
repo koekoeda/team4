2fc115603bc1a44334edcfc4366d08c226b80185
ichimi0730 <19084@hakodate.kosen-ac.jp>
2023-11-16 09:08:00 +0900

    画像の変更

--- game4/MyWorld.java
+++ game4/MyWorld.java
@@ -8,13 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
-    {    
-        
+    {
+        super(1000,667,1);
+        GreenfootImage Sgmap = new GreenfootImage( "images/Sgmap.png" );
+        getBackground().drawImage( Sgmap, 100, 100 );
+        Sgmap.scale( 400, 700 );
     }
 }
