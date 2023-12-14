eee5ca0cee2fcf4aa2ca77648f383c47f49e4ac0
HosinoYozoraP <19039@hakodate.kosen-ac.jp>
2023-12-14 09:04:38 +0900

    更新

--- game4/MyWorld.java
+++ game4/MyWorld.java
@@ -14,45 +14,8 @@ public class MyWorld extends World
         Sgmap.scale(800, 500);
         getBackground().drawImage( Sgmap, 100, 50 );
         
-
-        String input = Greenfoot.ask("プレイヤー人数を入力してください。");
-        int players=Integer.parseInt(input);
+        String input
         
-        showText(input, 400, 200);
-        
-        switch(players){
-            case 1:
-            input = Greenfoot.ask("2人以上を入力してください。\nプレイヤー人数を入力してください。");
-            players=Integer.parseInt(input);
-            break;
-            case 2:
-             addObject(new kaguya5(),150,500);
-             addObject(new kaguya4(),165,500);
-             break;
-            case 3:
-             addObject(new kaguya5(),150,500);
-             addObject(new kaguya4(),165,500);
-             addObject(new kaguya3(),180,500);
-             break;
-            case 4:
-             addObject(new kaguya5(),150,500);
-             addObject(new kaguya4(),165,500);
-             addObject(new kaguya3(),180,500);
-             addObject(new kaguya2(),195,500);
-             break;
-            case 5:
-             addObject(new kaguya5(),150,500);
-             addObject(new kaguya4(),165,500);
-             addObject(new kaguya3(),180,500);
-             addObject(new kaguya2(),195,500);
-             addObject(new kaguya1(),210,500);
-             break;
-            default:
-             input = Greenfoot.ask("5人以下」を入力してください。\nプレイヤー人数を入力してください。");
-             players=Integer.parseInt(input);
-             break;
-            
-        }
         
         /* 好感度のパラメータ表示Sample */
         showText("koukando",60,20);
