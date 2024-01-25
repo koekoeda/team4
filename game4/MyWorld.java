dfcbee0aa541b3726307eca6b8428a8ef0ef3980
ichimi0730 <19084@hakodate.kosen-ac.jp>
2024-01-25 09:15:31 +0900

    更新

--- game4/MyWorld.java
+++ game4/MyWorld.java
@@ -1,13 +1,14 @@
 import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
 import java.util.Random;
 
 public class MyWorld extends World{
     public GreenfootImage Sgmap = null;
+    public GreenfootSound Sound = null;
     public GreenfootImage Meter = null;
     public GreenfootImage MeterBox = null;
     public int count = 0;
     public int kesi = 0;
-
     public int poi = 0;
     public int[] K_x = {200,185,175,165,150};
     public int[] K_y = {500,500,500,500,500}; //kaguyaの現在地
@@ -20,41 +21,44 @@ public class MyWorld extends World{
     public String inp = null;
     public boolean seigyo = true;
     public static boolean koro = true;    
-
     public void input(){
         while(seigyo){
             inp = Greenfoot.ask("プレイヤー人数を入力してください。\n2~5人を選択してください。");
             switch(inp){
                 case "2":
-                    addObject(new kaguya1(),K_x[0],K_y[0]);
-                    addObject(new kaguya2(),K_x[1],K_y[1]);
-                    jun_max = 2;
-                    seigyo=false;
-                    break;
+                addObject(new kaguya1(),K_x[0],K_y[0]);
+                addObject(new kaguya2(),K_x[1],K_y[1]);
+                jun_max = 2;
+                seigyo=false;
+                Sound.playLoop();
+                break;
                 case "3":
-                    addObject(new kaguya1(),K_x[0],K_y[0]);
-                    addObject(new kaguya2(),K_x[1],K_y[1]);
-                    addObject(new kaguya3(),K_x[2],K_y[2]);
-                    jun_max = 3;
-                    seigyo=false;
-                    break;
+                addObject(new kaguya1(),K_x[0],K_y[0]);
+                addObject(new kaguya2(),K_x[1],K_y[1]);
+                addObject(new kaguya3(),K_x[2],K_y[2]);
+                jun_max = 3;
+                seigyo=false;
+                Sound.playLoop();
+                break;
                 case "4":
-                    addObject(new kaguya1(),K_x[0],K_y[0]);
-                    addObject(new kaguya2(),K_x[1],K_y[1]);
-                    addObject(new kaguya3(),K_x[2],K_y[2]);
-                    addObject(new kaguya4(),K_x[3],K_y[3]);
-                    jun_max = 4;
-                    seigyo=false;
-                    break;
+                addObject(new kaguya1(),K_x[0],K_y[0]);
+                addObject(new kaguya2(),K_x[1],K_y[1]);
+                addObject(new kaguya3(),K_x[2],K_y[2]);
+                addObject(new kaguya4(),K_x[3],K_y[3]);
+                jun_max = 4;
+                seigyo=false;
+                Sound.playLoop();
+                break;
                 case "5":
-                    addObject(new kaguya1(),K_x[0],K_y[0]);
-                    addObject(new kaguya2(),K_x[1],K_y[1]);
-                    addObject(new kaguya3(),K_x[2],K_y[2]);
-                    addObject(new kaguya4(),K_x[3],K_y[3]);
-                    addObject(new kaguya5(),K_x[4],K_y[4]);
-                    jun_max = 5;
-                    seigyo=false;
-                    break;
+                addObject(new kaguya1(),K_x[0],K_y[0]);
+                addObject(new kaguya2(),K_x[1],K_y[1]);
+                addObject(new kaguya3(),K_x[2],K_y[2]);
+                addObject(new kaguya4(),K_x[3],K_y[3]);
+                addObject(new kaguya5(),K_x[4],K_y[4]);
+                jun_max = 5;
+                seigyo=false;
+                Sound.playLoop();
+                break;
             }
         }
     }
@@ -86,6 +90,7 @@ public class MyWorld extends World{
                 }
             }
         }
+
         if(koro == false){
             showText("spaceを押してください",200,250);
             if(Greenfoot.isKeyDown("space")){
@@ -101,11 +106,12 @@ public class MyWorld extends World{
         }
         int A1 = 1;
         int[] A2 = {3,5,7,9,21,23,25,27,29,41,43,45,47,49};//→上 14
-        int[] A3 = {2,4,6,8,22,24,26,28,42,44,46,48};//→下 12
+        int[] A3 = {2,4,6,8,22,24,26,28,42,44,46,48};//→下 1
         int[] A4 = {12,14,16,18,32,34,36,38};//←上 8
         int[] A5 = {11,13,15,17,19,31,33,35,37,39};//←下 10
         int[] A_up = {10,20,30,40};
         int Goal = 50;
+
         if(mae[i] <= sai[i]){
             showText("前 :"+mae[i]+", 最大 :"+sai[i],400,400);
             Greenfoot.delay(15);
@@ -154,6 +160,15 @@ public class MyWorld extends World{
         }
     }
 
+    public void event(){
+        int[] aka = {10};
+        for(int s = 0; s < 10; s++){
+            if(sai[i] == aka[s]){
+                showText("赤に止まりました",500,600);
+            }
+        }
+    }
+
     public void iti(){
         if(jun_max == 2){
             addObject(new kaguya1(),K_x[0],K_y[0]);
@@ -180,23 +195,23 @@ public class MyWorld extends World{
         }
     }
 
-    public void Kesi(){
-
-    }
-
-    public MyWorld()
-    {
+    public MyWorld(){
         super(1000,655,1);
         Sgmap = new GreenfootImage( "images/Sgmap.png" );
         Sgmap.scale(800, 500);
         getBackground().drawImage( Sgmap, 100, 50 );
+
+        Sound= new GreenfootSound("sounds/Moon_Shine.mp3");
+
+        Sound.setVolume(18);
+
     }
 
-    public void act()
-    {
+    public void act(){
         if(status==0){
             if(count == jun_max){
                 Greenfoot.stop();
+                Sound.stop();
             }
             input();
             status = 1;
@@ -212,6 +227,7 @@ public class MyWorld extends World{
                 //addObject(new saikoro(),300,300);
                 koro = true;
                 status = 2;
+
             }
         }
         else if(status == 2){
@@ -240,6 +256,7 @@ public class MyWorld extends World{
                             showText(""+count,500,500 );
                             if(count == jun_max){
                                 Greenfoot.stop();
+                                Sound.stop();
                                 status = 0;
                             }
                         }
@@ -263,7 +280,9 @@ public class MyWorld extends World{
         getBackground().drawImage( Meter, level, 50 );
         level += 20;
         }
-         */
-    }
-}
 
+         */
+
+    }
+
+}
